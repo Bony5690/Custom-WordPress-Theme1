2d710a50c2455674487fdb74fae423b2e8ad8e8f
Bony5690 <jerrybony@yahoo.com>
2017-05-15 06:16:09 -0500

    blog and modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,27 @@
-$(function(){
- // cache the window object
- var $window = $(window);
-
-  $('section[data-type="background"]').each(function(){
-
-    var $bgobj = $(this); // assigning the object
-    (window).scroll(function(){
-
-     //scroll the background at var speed
-     //the yPos is a negative value because we're scrolling it up
-
-    var yPos = -$(window.scrollTop() /
-    $bgobj.data('speed'));
-
-    var coords = '50%' + yPos + 'px';
-
-   // move the background
-
-   $bgobj.css({backgroundPosition: coords})
-
-   }); // end window scroll
-
-  });
+$(function() {
+	
+	// Cache the Window object
+	var $window = $(window);
+	
+	// Parallax Backgrounds
+	// Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
+	
+	$('section[data-type="background"]').each(function(){
+		var $bgobj = $(this); // assigning the object
+		
+		$(window).scroll(function() {
+		
+			// Scroll the background at var speed
+			// the yPos is a negative value because we're scrolling it UP!								
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+			
+			// Put together our final background position
+			var coords = '50% '+ yPos + 'px';
+			
+			// Move the background
+			$bgobj.css({ backgroundPosition: coords });
+			
+		}); // end window scroll
+	});
+	
 });
